0c150768a18e453a839ecf75b9ed93caa78b9d0f
JeanMarcoMiranda <elecord2014@gmail.com>
2026-03-06 20:23:32 -0500

    feat: adding task controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import type { Task } from './task.model';
 
-@Controller('task')
+@Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.taskService.createTask(title, description);
+  }
 }

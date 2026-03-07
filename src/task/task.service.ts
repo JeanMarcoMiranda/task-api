34a8aa5ee71a4bcbda0bdb8ceda78eeeb4426642
JeanMarcoMiranda <elecord2014@gmail.com>
2026-03-06 20:00:51 -0500

    feat: project setup, task service creation

---
+++ src/task/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class TaskService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: randomUUID(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
